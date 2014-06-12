d9c4c18badb27c9ab6f58c260f71a1b6ec78ca50
Kanishk Kunal <kanishk.kunal@hotmail.com>
2014-06-12 13:08:24 +0530

    Home Page Template

--- header.php
+++ header.php
@@ -69,8 +69,27 @@
             </nav>
 		<?php endif; ?>
     </div>
-
+     <?php if ( is_page_template( 'home-page.php' ) ) : ?>
+        <header id="masthead" class="jumbotron site-header text-center" role="banner">
+            <div class="container">
+                <?php
+			    $header_image = ot_get_option('custom-image') ? ot_get_option('custom-image') : developr_admin_gravatar();
+                $header_class = ot_get_option('clip-image') ? '' : 'img-circle' ;
+			    if ( ! empty( $header_image ) ) :
+		        ?>
+			        <a class="site-logo"  href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+				        <img src="<?php echo $header_image; ?>" alt="Gravatar" class="<?php echo $header_class; ?>" width="120" height="120" />
+			        </a>
+		        <?php endif; ?>
+		        <div class="site-branding text-center">
+                
+			            <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+                        <?php if ( !ot_get_option('site-description') ): ?><h2 class="site-description"><?php bloginfo( 'description' ); ?></h2><?php endif; ?>
+		        </div>
+            </div>
+	    </header><!-- #masthead -->
+    <?php endif; ?>
 	<div class="container">
         <div class="row">
-            <div id="content" class="col-md-8 col-sm-8">                    
+            <div id="content" class="<?php echo ( is_page_template( 'home-page.php' ) ) ? 'col-md-12' : 'col-md-8 col-sm-8' ;?>">                    
 	            <div class="site-content">
--- sidebar.php
+++ sidebar.php
@@ -7,6 +7,7 @@
 ?>
             </div><!--col-->    
         </div><!-- #content -->
+        <?php if ( !is_page_template( 'home-page.php' ) ) : ?>
         <div id="sidebar-content" class="col-md-4 col-sm-4">
             <div id="primary-sidebar" class="sidebar widget-area" role="complementary">
                 <div class="">
@@ -15,6 +16,7 @@
                 </div>
 	        </div><!-- #footer -->
         </div>
+        <?php endif; ?>
     </div><!--row-->
 </div><!--Container-->
 <div id="above-footer" class="widget-area">
