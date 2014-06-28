132c5009424e8fb272418bfc8c420c9daa659b0f
Kanishk Kunal <kanishk.kunal@hotmail.com>
2014-06-29 02:06:38 +0530

    IE fix, new cdn and change of colors

--- functions.php
+++ functions.php
@@ -142,13 +142,13 @@ add_action( 'widgets_init', 'developr_widgets_init' );
  */
 function developr_scripts() {
     wp_enqueue_style( 'open-sans', '//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,700italic,400,300,700');
-    wp_enqueue_style( 'raleway', '//fonts.googleapis.com/css?family=Raleway');
-    wp_enqueue_style( 'bootstrap', '//netdna.bootstrapcdn.com/bootswatch/3.1.1/yeti/bootstrap.min.css' );
-    wp_enqueue_style( 'font-awesome', '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css' );
-	wp_enqueue_style( 'human-droid-style', get_stylesheet_uri(), NULL, "0.8" );
+    wp_enqueue_style( 'varela-round', '//fonts.googleapis.com/css?family=Varela+Round');
+    wp_enqueue_style( 'bootstrap', '//maxcdn.bootstrapcdn.com/bootswatch/3.1.1/yeti/bootstrap.min.css' );
+    wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css' );
+	wp_enqueue_style( 'human-droid-style', get_stylesheet_uri(), NULL, "0.8.1" );
 
-    wp_enqueue_script( 'bootstrap-js', '//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js', array('jquery'), NULL, true );
-    wp_enqueue_script( 'scripts', get_template_directory_uri() . '/js/scripts.js', array( 'jquery' ),'0.8', true ); 
+    wp_enqueue_script( 'bootstrap-js', '//maxcdn.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js', array('jquery'), NULL, true );
+    wp_enqueue_script( 'scripts', get_template_directory_uri() . '/js/scripts.js', array( 'jquery' ),'0.8.1', true ); 
  
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
