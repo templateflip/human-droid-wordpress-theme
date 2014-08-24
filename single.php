e610cb132ff46152ddf0ab33dc23aa0afe81520f
Kanishk Kunal <kanishk.kunal@hotmail.com>
2014-08-24 22:55:12 +0530

    Adding author bio

--- single.php
+++ single.php
@@ -16,6 +16,15 @@ get_header(); ?>
 
             <?php if ( ot_get_option( 'post-nav' ) == 'content') { developr_post_nav(); } ?>
 
+            <?php if ( get_the_author_meta( 'description' ) ): ?>
+                <div class="author-bio">
+				    <div class="bio-avatar pull-right"><?php echo get_avatar(get_the_author_meta('user_email'),'128'); ?></div>
+                    <h4><strong>About the author</strong></h4>
+				    <p class="bio-desc"><?php the_author_meta('description'); ?></p>
+				    <div class="clear"></div>
+                </div>
+		    <?php endif; ?>
+
 			<?php
 				// If comments are open or we have at least one comment, load up the comment template
 				if ( comments_open() || '0' != get_comments_number() ) :
