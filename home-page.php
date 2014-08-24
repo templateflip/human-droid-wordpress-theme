51626abc64d09ae59d1a074c4e67c030f49e367f
Kanishk Kunal <kanishk.kunal@hotmail.com>
2014-08-25 00:14:44 +0530

    New boxed layout

--- home-page.php
+++ home-page.php
@@ -16,7 +16,7 @@ Template Name: Home Page (Full Width)
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="site-main site-page site-page-home" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
