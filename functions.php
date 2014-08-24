b314d650a6cf34719bab2b1d2a9dbc7fe6cf228a
Kanishk Kunal <kanishk.kunal@hotmail.com>
2014-08-24 19:47:27 +0530

    All fonts to Open Sans

--- functions.php
+++ functions.php
@@ -141,8 +141,11 @@ add_action( 'widgets_init', 'developr_widgets_init' );
  * Enqueue scripts and styles.
  */
 function developr_scripts() {
-    wp_enqueue_style( 'open-sans', '//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,700italic,400,300,700');
-    wp_enqueue_style( 'raleway', '//fonts.googleapis.com/css?family=Raleway:400,500');
+    //wp_enqueue_style( 'open-sans', '//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,700italic,400,300,700');
+    //wp_enqueue_style( 'raleway', '//fonts.googleapis.com/css?family=Raleway:400,500');
+    //wp_enqueue_style( 'roboto-condensed', '//fonts.googleapis.com/css?family=Roboto+Condensed');
+    //wp_enqueue_style( 'roboto', '//fonts.googleapis.com/css?family=Roboto:300,400,600');
+    //wp_enqueue_style( 'roboto-slab', '//fonts.googleapis.com/css?family=Roboto+Slab');
     wp_enqueue_style( 'bootstrap', '//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/yeti/bootstrap.min.css' );
     wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css' );
 	wp_enqueue_style( 'human-droid-style', get_stylesheet_uri(), NULL, "0.8.2" );
