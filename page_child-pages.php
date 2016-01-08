bb811976b3521b2c0c6cda5e17fa12650a45e133
Kanishk Kunal <kanishk.kunal@hotmail.com>
2016-01-08 21:43:28 +0530

    Child pages template

---
+++ page_child-pages.php
@@ -0,0 +1,27 @@
+<?php /* Template Name: Child Page Template */
+
+
+add_action('beans_primary_before_markup', 'human_droid_display_child_pages_menu');
+
+function human_droid_display_child_pages_menu() {
+  $childpages = get_pages( array( 'child_of' => wp_get_post_parent_id( get_the_ID() ) ) );
+
+  if(count($childpages) > 1) {
+    echo '<div class="uk-button-group" style="width:100%;">';
+    $width = 100.0 / count($childpages);
+    foreach( $childpages as $page ) {
+      $page_class = "primary-ghost-buton";
+      if( $page->ID ==  get_the_ID() ) {
+        $page_class = "primary-ghost-buton primary-ghost-buton-active";
+      }
+  	?>
+  		<a class="uk-button uk-button-large <?php echo $page_class; ?>" style="width:<?php echo $width; ?>%" href="<?php echo get_page_link( $page->ID ); ?>"><?php echo $page->post_title; ?></a>
+  	<?php
+  	}
+    echo '</div>';
+  }
+
+}
+
+// Load beans document
+beans_load_document();
