abffc015403e9eaf356f1b561091d074103b95e0
Kanishk Kunal <kanishk.kunal@hotmail.com>
2016-01-09 21:53:19 +0530

    Sort child pages menu by menu order

--- page_child-pages.php
+++ page_child-pages.php
@@ -4,7 +4,7 @@
 add_action('beans_primary_before_markup', 'human_droid_display_child_pages_menu');
 
 function human_droid_display_child_pages_menu() {
-  $childpages = get_pages( array( 'child_of' => wp_get_post_parent_id( get_the_ID() ) ) );
+  $childpages = get_pages( array( 'child_of' => wp_get_post_parent_id( get_the_ID()), 'sort_column' => 'menu_order'  ) );
 
   if(count($childpages) > 1) {
     echo '<div class="tm-ghost-button-group" style="width:100%;">';
