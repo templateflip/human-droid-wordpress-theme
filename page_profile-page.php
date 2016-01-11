d660b0d85ea70a35ae682cace338ad4d1c2bd2d2
Kanishk Kunal <kanishk.kunal@hotmail.com>
2016-01-11 23:06:14 +0530

    Profile when images is smaller than width

--- page_profile-page.php
+++ page_profile-page.php
@@ -6,7 +6,19 @@ function human_droid_profile_setup_document() {
   beans_remove_attribute('beans_main_grid', 'class', 'uk-panel uk-panel-box uk-panel-space');
 
   beans_add_attribute('beans_post', 'class', 'tm-profile-main uk-overlay');
-  beans_add_attribute('beans_post_header', 'class', 'uk-overlay-panel uk-overlay-background');
+
+  beans_add_attribute('beans_post_header', 'class', 'uk-overlay-panel');
+
+  //Add an overlay panel only if image is larger than full width
+  $tn_id = get_post_thumbnail_id( get_the_ID() );
+  $img = wp_get_attachment_image_src( $tn_id, 'full' );
+  $width = $img[1];
+  if($width >= 830) {
+    beans_add_attribute('beans_post_header', 'class', 'uk-overlay-background');
+  }
+  else {
+    beans_add_attribute('beans_post_header', 'class', 'tm-light-background');
+  }
 }
 
 beans_add_smart_action('beans_post_header_prepend_markup', 'human_droid_profile_header');
